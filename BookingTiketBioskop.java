84bcdf71866839a7134905764992f2b838f30415
siskanuriaprilia <aprilias878@gmail.com>
2023-12-05 14:59:09 +0700

    modifikasi terbaru

--- BookingTiketBioskop.java
+++ BookingTiketBioskop.java
@@ -396,17 +396,24 @@ public class BookingTiketBioskop {
     }
     static void PemilihanFILM(){
         // Pilih Film
-        System.out.println("Daftar Film yang Tersedia:");
-        for (int i = 0; i < jdl_film.length; i++) {
-            System.out.println((i + 1) + ". " + jdl_film[i]);
+        System.out.println("|==============================|");
+        System.out.println("|Daftar Film yang Tersedia:    |");
+        System.out.println("|==============================|");
+        for (int i = 0; i < jdl_film.length; i++) 
+        {
+        System.out.println("|"+(i + 1) + ". " + jdl_film[i]);
         }
-        System.out.print("Pilih nomor film yang ingin Anda tonton: ");
+        System.out.print(  "|Pilih nomor film yang ingin Anda tonton: ");
         jdlinput = inputUntukJdlFilm.nextInt();
         if (jdlinput >= 1 && jdlinput <= jdl_film.length) {
-            jdlinput--; // Mengurangi 1 karena array dimulai dari 0
-            System.out.println("Film yang Anda pilih: " + jdl_film[jdlinput]);
-        } else {
-            System.out.println("Nomor film yang Anda masukkan tidak valid.");
+        jdlinput--; // Mengurangi 1 karena array dimulai dari 0
+        System.out.println(  "|Film yang Anda pilih: " + jdl_film[jdlinput] +"|");
+        } 
+        else 
+        {
+        System.out.println("|===========================================|");
+        System.out.println("|Nomor film yang Anda masukkan tidak valid. |");
+        System.out.println("|===========================================|");
         }       
     }
     static void PemilihanStudioWish(){
@@ -415,27 +422,24 @@ public class BookingTiketBioskop {
         System.out.println("|Daftar Studio yang Tersedia:  |");
         System.out.println("|==============================|");
         for (int i = 0; i < studioWISH.length; i++) {
-        System.out.println("|==============================|");
         System.out.println(( "|" + i + 1) + ". " + studioWISH[i] + " (Harga: " + hrg_stdWISH[i] + ")"+ "     |");
         System.out.println("|==============================|");
         }
-        System.out.println("|==============================|");
-        System.out.println("|Pilih nomor studio:           |");
-        System.out.println("|==============================|");
+        System.out.print(  "|Pilih nomor studio: ");
         studioInput = inputUntukStudio.nextInt();
         if (studioInput >= 1 && studioInput <= studioWISH.length) 
         {
         studioInput--; // Mengurangi 1 karena array dimulai dari 0
-        System.out.println("|===============================|");
-        System.out.println(  "|Studio yang Anda pilih: " + studioWISH[studioInput]+" |");
-        System.out.println("|===============================|");
+        System.out.println("|==============================|");
+        System.out.println(  "|Studio yang Anda pilih: " + studioWISH[studioInput]+"|");
+        System.out.println("|==============================|");
         } 
         else 
         {
-        System.out.println("|===============================================|");
-        System.out.println("|Nomor studio yang Anda masukkan tidak valid.   |");
-        System.out.println("|===============================================|");
-        }
+        System.out.println("|============================================|");
+        System.out.println("|Nomor studio yang Anda masukkan tidak valid.|");
+        System.out.println("|============================================|");
+        } 
     }
     static void PemilihanStudioTheMarvels(){
         // Pilih Studio
@@ -444,30 +448,51 @@ public class BookingTiketBioskop {
         System.out.println("|==============================|");
         for (int i = 0; i < studioTheMarvels.length; i++) 
         {
-        System.out.println("|==========================|");
-        System.out.println(( "|" + i + 1) + ". " + studioTheMarvels[i] + " (Harga: " + hrg_stdTheMarvels[i] + ")|");
-        System.out.println("|==========================|");
+        System.out.println(  "|" + (i + 1) + ". " + studioTheMarvels[i] + " (Harga: " + hrg_stdTheMarvels[i] +")      |");
+        System.out.println("|==============================|");
         }
-        System.out.println("|==============================|");
-        System.out.println("|Pilih nomor studio:           |");
-        System.out.println("|==============================|");
+        System.out.print(  "|Pilih nomor studio: ");
         studioInput = inputUntukStudio.nextInt();
         if (studioInput >= 1 && studioInput <= studioTheMarvels.length) 
         {
         studioInput--; // Mengurangi 1 karena array dimulai dari 0
-        System.out.println("|==========================================================================|");
-        System.out.println("  |Studio yang Anda pilih: " + studioTheMarvels[studioInput]+               "|");
-        System.out.println("|==========================================================================|");  
+        System.out.println("|===============================|");
+        System.out.println(  "|Studio yang Anda pilih: " + studioTheMarvels[studioInput] +  "   |");
+        System.out.println("|===============================|"); 
         } 
         else 
         {
-        System.out.println("|===============================|");
+        System.out.println("|============================================|");
         System.out.println("|Nomor studio yang Anda masukkan tidak valid.|");
-        System.out.println("|===============================|");
+        System.out.println("|============================================|");
         }
     }
- 
-    
+    static void PemilihanStudioNapoleon(){
+        // Pilih 
+        System.out.println("|==============================|");
+        System.out.println("|Daftar Studio yang Tersedia:  |");
+        System.out.println("|==============================|");
+        for (int i = 0; i < studioNapoleon.length; i++) 
+        {
+        System.out.println(( "|"+(i + 1) + ". " + studioNapoleon[i] + " (Harga: " + hrg_stdNapoleon[i])+" |");
+        System.out.println("|==============================|");
+        }
+        System.out.print("|Pilih nomor studio: ");
+        studioInput = inputUntukStudio.nextInt();
+        if (studioInput >= 1 && studioInput <= studioNapoleon.length) 
+        {
+        studioInput--; // Mengurangi 1 karena array dimulai dari 0
+        System.out.println("|====================================|");
+        System.out.println(  "|Studio yang Anda pilih: " + studioNapoleon[studioInput]+"|");
+        System.out.println("|====================================|");
+        } 
+        else 
+        {
+        System.out.println("|============================================|");
+        System.out.println("|Nomor studio yang Anda masukkan tidak valid.|");
+        System.out.println("|============================================|");
+        }
+    }
     static void fillArrayWithZero(char[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
